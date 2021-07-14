2f1d93d6d5ec035da4a30d45fe13d2ebdf3e73fa
eduwr <eduwric@gmail.com>
2021-07-13 23:23:18 -0300

    feat: checks if the message can be change

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,6 +7,7 @@ const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = "Hi there";
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -17,7 +18,7 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["Hi there"],
+      arguments: [INITIAL_MESSAGE],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
@@ -30,6 +31,14 @@ describe("Inbox", () => {
 
   it("has a default message", async () => {
     const message = await inbox.methods.message().call();
-    assert.strictEqual(message, "Hi there");
+    assert.strictEqual(message, INITIAL_MESSAGE);
+  });
+
+  it("can change the message", async () => {
+    const NEW_MESSAGE = "Bye there";
+    await inbox.methods.setMessage(NEW_MESSAGE).send({ from: accounts[0] });
+
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, NEW_MESSAGE);
   });
 });
