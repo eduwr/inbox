ca08c3442ffdb4789adcd04fd8996a0ea423725f
eduwr <eduwric@gmail.com>
2021-07-13 23:15:35 -0300

    feat: test if there is a default message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,13 +1,35 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
+const { isRegExp } = require("util");
 const Web3 = require("web3");
-
+const { interface, bytecode } = require("../compile");
 const web3 = new Web3(ganache.provider());
 
-describe("smoke test", () => {
-  it("1 + 1 should be 2", () => {
-    const sum = 1 + 1;
+let accounts;
+let inbox;
 
-    assert.strictEqual(sum, 2);
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy the contract
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    // checks if the address exists
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, "Hi there");
   });
 });
